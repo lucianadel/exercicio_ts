a127af0b9fc03be09c067b9c3d75c365492a4c26
Luciana <luciana342010@hotmail.com>
2024-10-30 16:07:44 -0300

    Subindo tarefa typescript

---
+++ src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+function saudacao(nome) {
+    return `Olá, ${nome}!`;
+}
+// Exemplos de uso:
+const resultadoMultiplicacao = multiplicar(5, 3);
+console.log(resultadoMultiplicacao);
+const saudacaoPersonalizada = saudacao("Luna");
+console.log(saudacaoPersonalizada);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function saudacao(nome: string): string {
+    return `Olá, ${nome}!`;
+}
+
+// Exemplos de uso:
+const resultadoMultiplicacao = multiplicar(5, 3);
+console.log(resultadoMultiplicacao); 
+
+const saudacaoPersonalizada = saudacao("Luna");
+console.log(saudacaoPersonalizada); 
